ede36633d31c11df3de96c9269f2683a7d21e271
Robson <relative@gmail.com>
2021-03-09 10:21:12 +0000

    Automated commit

--- code/Modules/Astronomy/template.js
+++ code/Modules/Astronomy/template.js
@@ -6,34 +6,36 @@ var imageData = {
 }
 
 function word() {
-	var box = makeBox(2, 2);
-	box
-		.append('div')
-		.attr('class', 'title')
-		.html('&#x1F6F8;<a href="https://apod.nasa.gov/apod/astropix.html">Astronomy Picture of the Day</a>')
-	box = box
-		.append('div')
-		.attr('class', 'inside');	
-	var inside = box
-		.append('div');
-	inside
-		.append('p')
-		.append('a')
-		.attr('href', imageData.LinkImage)
-		.append('img')
-		.style('width', 'auto')
-		.style('height', 'auto')
-		.style('max-width', '400px')
-		.style('max-height', '320px')
-		.attr('src', imageData.Source);
-	inside
-		.append('p')
-		.html('&nbsp;');
-	inside
-		.append('p')
-		.append('a')
-		.attr('href', imageData.LinkPage)
-		.html(imageData.Title);
+	if (!imageData.Source.endsWith('/')) {
+		var box = makeBox(2, 2);
+		box
+			.append('div')
+			.attr('class', 'title')
+			.html('&#x1F6F8;<a href="https://apod.nasa.gov/apod/astropix.html">Astronomy Picture of the Day</a>')
+		box = box
+			.append('div')
+			.attr('class', 'inside');
+		var inside = box
+			.append('div');
+		inside
+			.append('p')
+			.append('a')
+			.attr('href', imageData.LinkImage)
+			.append('img')
+			.style('width', 'auto')
+			.style('height', 'auto')
+			.style('max-width', '400px')
+			.style('max-height', '320px')
+			.attr('src', imageData.Source);
+		inside
+			.append('p')
+			.html('&nbsp;');
+		inside
+			.append('p')
+			.append('a')
+			.attr('href', imageData.LinkPage)
+			.html(imageData.Title);
+	}
 }
 
 modules.push(word);
--- docs/modules/astronomy.js
+++ docs/modules/astronomy.js
@@ -1,39 +1,41 @@
 var imageData = {
-	Title:     ' Perseverance 360: Unusual Rocks and the Search for Life on Mars ',
+	Title:     'Perseverance 360: Unusual Rocks and the Search for Life on Mars',
 	Source:    'https://apod.nasa.gov/apod/',
 	LinkImage: 'https://apod.nasa.gov/apod/',
 	LinkPage:  'https://apod.nasa.gov/apod/ap210309.html'
 }
 
 function word() {
-	var box = makeBox(2, 2);
-	box
-		.append('div')
-		.attr('class', 'title')
-		.html('&#x1F6F8;<a href="https://apod.nasa.gov/apod/astropix.html">Astronomy Picture of the Day</a>')
-	box = box
-		.append('div')
-		.attr('class', 'inside');	
-	var inside = box
-		.append('div');
-	inside
-		.append('p')
-		.append('a')
-		.attr('href', imageData.LinkImage)
-		.append('img')
-		.style('width', 'auto')
-		.style('height', 'auto')
-		.style('max-width', '400px')
-		.style('max-height', '320px')
-		.attr('src', imageData.Source);
-	inside
-		.append('p')
-		.html('&nbsp;');
-	inside
-		.append('p')
-		.append('a')
-		.attr('href', imageData.LinkPage)
-		.html(imageData.Title);
+	if (!imageData.Source.endsWith('/')) {
+		var box = makeBox(2, 2);
+		box
+			.append('div')
+			.attr('class', 'title')
+			.html('&#x1F6F8;<a href="https://apod.nasa.gov/apod/astropix.html">Astronomy Picture of the Day</a>')
+		box = box
+			.append('div')
+			.attr('class', 'inside');
+		var inside = box
+			.append('div');
+		inside
+			.append('p')
+			.append('a')
+			.attr('href', imageData.LinkImage)
+			.append('img')
+			.style('width', 'auto')
+			.style('height', 'auto')
+			.style('max-width', '400px')
+			.style('max-height', '320px')
+			.attr('src', imageData.Source);
+		inside
+			.append('p')
+			.html('&nbsp;');
+		inside
+			.append('p')
+			.append('a')
+			.attr('href', imageData.LinkPage)
+			.html(imageData.Title);
+	}
 }
 
 modules.push(word);
