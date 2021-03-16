820f1149e88da6c69b77c9d27f04d631ef98fe50
Robson <relative@gmail.com>
2021-03-16 21:21:49 +0000

    Automated commit

--- code/Program.cs
+++ code/Program.cs
@@ -24,6 +24,7 @@
             new Sun(),
             new Twitter(),
             new Vanguard(),
+            new VanguardChart(),
             new Weather(),
             new Word(),
             new YouTube()
